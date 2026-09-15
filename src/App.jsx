f5e9531e08fd3db25419dae552aaffd1783b489d
vranulfo <viniciusranulfo@gmail.com>
2026-09-15 19:07:40 -0300

    chore: initial project setup

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import heroImg from './assets/hero.png'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div className="min-h-screen bg-slate-950 text-white flex items-center justify-center">
+      <h1 className="text-4xl font-bold">
+        Desafio Fitness
+      </h1>
+    </div>
+  )
+}
+
+export default App
